135ee1ce388fe78cdbd9eecafb8ea346a66167d3
mosfiqurrahmanjsd <nisho.mushfiq2020@gmil.com>
2024-05-03 00:42:33 +0600

    added seat left seat updated seat added

--- js/index.js
+++ js/index.js
@@ -1,48 +1,62 @@
 
 
-
-
-
-
-
+const allClickableSeat = document.getElementsByClassName('clickable-btn');
+const seletedSeatElement = document.getElementById('seletedSeat');
 const leftSeatElement = document.getElementById('leftSeat');
 const addedSeatElement = document.getElementById('seat');
-const seletedSeatElement = document.getElementById('seletedSeat');
-
-function handleClick(event) {
-
-    // seat background color change
-    event.target.style.backgroundColor = '#1DD100';
-    event.target.style.color = 'white';
-
-
-    // left seat 
-    const avalableSeat = leftSeatElement.innerText - 1;
-    leftSeatElement.innerText = avalableSeat;
-
-
-    // added seat 
-    const addSeat = addedSeatElement.innerText;
-    const addedSeatCount = parseFloat(addSeat);
-    addedSeatElement.innerText = addedSeatCount + 1;
 
 
 
+for (const clickableSeat of allClickableSeat) {
+    var seatCollection = [];
+    clickableSeat.addEventListener('click', function (event) {
+        const seatNumber = event.target.innerText;
+        const seatClass = 'Economoy';
+        const seatPrice = 550;
+
+        if (seatCollection.includes(seatNumber)) {
+            alert('Seat Adready Aded!')
+        } else {
+            event.target.style.backgroundColor = '#1DD100';
+            event.target.style.color = 'white';
+            const tr = document.createElement('tr');
+            tr.classList.add('border-none')
+            tr.innerHTML = `
+                <td class="seat-number">${seatNumber}</td>
+                <td>${seatClass}</td>
+                <td class="ps-14">${seatPrice}</td>
+            `
+            seletedSeatElement.appendChild(tr);
+            seatCollection.push(seatNumber);
 
 
-    const tr = document.createElement('tr');
-    tr.classList.add('border-none')
-    tr.innerHTML = `
-        <td>${event.target.innerText}</td>
-        <td>Economoy</td>
-        <td>550</td>
-    `
-
-    seletedSeatElement.appendChild(tr);
+            // availabe seat 
+            const avalableSeat = getElementValueById('leftSeat');
+            leftSeatElement.innerText = avalableSeat - 1;
 
 
+            // add seat 
+            const addedSeat = getElementValueById('seat');
+            addedSeatElement.innerText = addedSeat + 1;
 
 
+        }
 
+
+    })
 }
+
+
+
+
+function getElementValueById(elementId) {
+    const elementValue = document.getElementById(elementId).innerText;
+    const numberValue = parseInt(elementValue);
+    return numberValue;
+}
+
+
+
+
+
 
