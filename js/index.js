43b5e4ba2defddf1549965caf36088338c95747e
mosfiqurrahmanjsd <nisho.mushfiq2020@gmil.com>
2024-05-10 06:47:58 +0600

    updated next button

--- js/index.js
+++ js/index.js
@@ -1,10 +1,38 @@
-
-
 const allClickableSeat = document.getElementsByClassName('clickable-btn');
 const seletedSeatElement = document.getElementById('seletedSeat');
 const leftSeatElement = document.getElementById('leftSeat');
 const addedSeatElement = document.getElementById('seat');
+const phoneInput = document.getElementById('phone');
+const nextBtn = document.getElementById('next-btn');
 
+
+
+
+// navigate to ticket section smooth scrool
+function navigatSection(sectionId) {
+    const section = document.getElementById(sectionId);
+    section.scrollIntoView({ behavior: 'smooth' });
+}
+
+
+// next button enable
+let totalSeat = 0;
+function nextBtnEnable() {
+    const selectedSeat = totalSeat > 0;
+    const validPhone = phoneInput.value.trim() !== '';
+    nextBtn.classList.toggle(
+        "btn-disabled",
+        !(selectedSeat && validPhone)
+    );
+
+    if (nextBtn.classList.contains("btn-disabled")) {
+        nextBtn.style.backgroundColor = '#F7F8F8';
+        nextBtn.style.color = '#7D8085';
+    } else {
+        nextBtn.style.backgroundColor = '#1DD100';
+        nextBtn.style.color = 'white';
+    }
+}
 
 
 for (const clickableSeat of allClickableSeat) {
@@ -17,6 +45,7 @@ for (const clickableSeat of allClickableSeat) {
         if (seatCollection.includes(seatNumber)) {
             alert('Seat Adready Aded!')
         } else {
+
             // availabe seat count
             const avalableSeat = getElementValueById('leftSeat');
             leftSeatElement.innerText = avalableSeat - 1;
@@ -36,18 +65,25 @@ for (const clickableSeat of allClickableSeat) {
             tr.innerHTML = `
                 <td class="seat-number">${seatNumber}</td>
                 <td>${seatClass}</td>
-                <td class="ps-14">${seatPrice}</td>
+                <td class="price ps-14">${seatPrice}</td>
             `
             seletedSeatElement.appendChild(tr);
             seatCollection.push(seatNumber);
 
+            totalSeat = seatCollection.length;
+
             updateTotalPrice(seatPrice);
+            updateGrandTotal(seatPrice);
+
+            nextBtnEnable();
 
         }
 
-
     })
 }
+
+
+phoneInput.addEventListener('input', nextBtnEnable);
 
 
 
@@ -56,6 +92,11 @@ for (const clickableSeat of allClickableSeat) {
 function updateTotalPrice(value) {
     const totalPrice = getElementValueById('total-price');
     document.getElementById('total-price').innerText = totalPrice + value;
+}
+
+function updateGrandTotal(value) {
+    const grandTotalPrice = getElementValueById('grand-total');
+    document.getElementById('grand-total').innerText = grandTotalPrice + value;
 }
 
 
@@ -67,8 +108,3 @@ function getElementValueById(elementId) {
     const numberValue = parseInt(elementValue);
     return numberValue;
 }
-
-
-
-
-
